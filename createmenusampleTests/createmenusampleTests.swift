f727484713e7c17961627e7643eceb28b1a36039
Ron Rith <ronrith@rons-mac.local>
2018-04-29 18:33:20 -0700

    Initial Commit

---
+++ createmenusampleTests/createmenusampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  createmenusampleTests.swift
+//  createmenusampleTests
+//
+//  Created by Ron Rith on 4/29/18.
+//  Copyright © 2018 Ron Rith. All rights reserved.
+//
+
+import XCTest
+@testable import createmenusample
+
+class createmenusampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
